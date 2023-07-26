1dd40e342915af2e56d4191de21d55838c8416bc
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-07-26 19:43:25 +0300

    Added homework 3 solution of section 4 data types and variables

---
+++ homework/section-4-data-types-and-variables/homework-3/homework-3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void problem_1() {
+  // Swapping 3 numbers
+  int num1, num2, num3, temp = -1;
+
+  cin >> num1 >> num2 >> num3;
+
+  temp = num1;
+  num1 = num2;
+  num2 = num3;
+  num3 = temp;
+
+  cout << num1 << ' ' << num2 << ' ' << num3 << '\n';
+}
+
+void problem_2() {
+  // Print Me
+  int a, b;
+  cin >> a >> b;
+
+  int coeff = 0.5 * (b + 1);
+  int result = (a * a) * coeff + (2 * a + 1) * (1 - coeff);
+  cout << result << endl;
+}
+
+void problem_3() {
+  // Sum numbers from 1 to N
+  int num;
+  cin >> num;
+
+  cout << (num * (num + 1)) * 0.5 << endl;
+}
+
+int main() {
+  problem_1();
+  problem_2();
+  problem_3();
+
+  return 0;
+}
