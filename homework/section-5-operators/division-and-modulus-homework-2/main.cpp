28a3ae0e21ec184bb2c2bca2643f951deecb2182
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-07-26 20:34:40 +0300

    Added division and modulus homework 2 solution of section 5 operators

---
+++ homework/section-5-operators/division-and-modulus-homework-2/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void problem_1() {
+  // Is even?
+  int num;
+  cin >> num;
+
+  // Is even using %2
+  bool is_even1 = num % 2 == 0;
+
+  // Is even using /2
+  bool is_even2 = num - ((num / 2) * 2) == 0;
+
+  // Is even using %10
+  int reminder = num % 10;
+  bool is_even3 = reminder == 0 || reminder == 2 || reminder == 4 ||
+                  reminder == 6 || reminder == 8;
+}
+
+void problem_2() {
+  // Last 3 digits sum
+  int num, sum{0};
+  cin >> num;
+
+  // 1st iteration
+  sum += num % 10;
+  num /= 10;
+
+  // 2nd iteration
+  sum += num % 10;
+  num /= 10;
+
+  // 3rd iteration
+  sum += num % 10;
+  num /= 10;
+
+  cout << sum << endl;
+}
+
+void problem_3() {
+  // 4th digits from the end
+  int num;
+  cin >> num;
+
+  int result = (num / 1000) % 10;
+  cout << result;
+}
+
+int main() {
+  problem_1();
+  problem_2();
+  problem_3();
+
+  return 0;
+}
