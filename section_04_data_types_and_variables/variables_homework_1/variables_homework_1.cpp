106c16cb187d1410f52d4844a51a5aa4174ceb35
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-17 19:33:17 +0300

    Add variables homework 1 - problem 1 solution

---
+++ section_04_data_types_and_variables/variables_homework_1/variables_homework_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void problem_1()
+{
+    // Math operations
+    int a, b;
+    cin >> a >> b;
+
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+}
+
