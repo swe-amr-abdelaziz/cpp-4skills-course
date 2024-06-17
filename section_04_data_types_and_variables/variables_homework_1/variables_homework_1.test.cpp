1a5c80c888ee56a109cfe49a34942956c571ab34
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-17 19:37:11 +0300

    Add variables homework 1 - problem 1 unit testing

--- section_04_data_types_and_variables/variables_homework_1/variables_homework_1.test.cpp
+++ section_04_data_types_and_variables/variables_homework_1/variables_homework_1.test.cpp
@@ -13,3 +13,53 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Problem1Test, input_even_odd_numbers_output_basic_math_operations)
+{
+    string output = getFunctionOutput(problem_1, "12 3");
+    EXPECT_EQ(output,
+              "12 + 3 = 15\n"
+              "12 - 3 = 9\n"
+              "12 / 3 = 4\n"
+              "12 * 3 = 36\n");
+}
+
+TEST(Problem1Test, input_even_even_numbers_output_basic_math_operations)
+{
+    string output = getFunctionOutput(problem_1, "12 6");
+    EXPECT_EQ(output,
+              "12 + 6 = 18\n"
+              "12 - 6 = 6\n"
+              "12 / 6 = 2\n"
+              "12 * 6 = 72\n");
+}
+
+TEST(Problem1Test, input_odd_odd_numbers_output_basic_math_operations)
+{
+    string output = getFunctionOutput(problem_1, "7 3");
+    EXPECT_EQ(output,
+              "7 + 3 = 10\n"
+              "7 - 3 = 4\n"
+              "7 / 3 = 2\n"
+              "7 * 3 = 21\n");
+}
+
+TEST(Problem1Test, input_negative_positive_numbers_output_basic_math_operations)
+{
+    string output = getFunctionOutput(problem_1, "-12 3");
+    EXPECT_EQ(output,
+              "-12 + 3 = -9\n"
+              "-12 - 3 = -15\n"
+              "-12 / 3 = -4\n"
+              "-12 * 3 = -36\n");
+}
+
+TEST(Problem1Test, input_negative_negative_numbers_output_basic_math_operations)
+{
+    string output = getFunctionOutput(problem_1, "-12 -3");
+    EXPECT_EQ(output,
+              "-12 + -3 = -15\n"
+              "-12 - -3 = -9\n"
+              "-12 / -3 = 4\n"
+              "-12 * -3 = 36\n");
+}
+
