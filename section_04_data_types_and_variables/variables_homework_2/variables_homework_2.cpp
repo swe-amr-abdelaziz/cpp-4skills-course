27e2c568c2c1e1f73b5860264415573397141099
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-18 12:21:46 +0300

    Add variables homework 2 - problem 2 solution

--- section_04_data_types_and_variables/variables_homework_2/variables_homework_2.cpp
+++ section_04_data_types_and_variables/variables_homework_2/variables_homework_2.cpp
@@ -19,3 +19,16 @@ void problem_1()
     // https://en.wikipedia.org/wiki/Fibonacci_number
 }
 
+void problem_2()
+{
+    // Swapping 2 numbers!
+    int num1, num2, num3 = -1;
+
+    cin >> num1 >> num2;
+
+    num3 = num1;
+    num1 = num2;
+    num2 = num3;
+
+    cout << num1 << " " << num2 << endl;
+}
