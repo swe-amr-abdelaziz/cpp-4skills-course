5a2654cc705b8390ee679a04cb2e86cc5e9b5fd0
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-18 11:30:09 +0300

    Add new unit test in printing homework
    Remove commented output and automate output validation using unit testing for homework 5

--- section_03_printing/printing_homework/printing_homework.h
+++ section_03_printing/printing_homework/printing_homework.h
@@ -2,3 +2,4 @@ void homework_1();
 void homework_2();
 void homework_3();
 void homework_4();
+void homework_5();
--- section_03_printing/printing_homework/printing_homework.test.cpp
+++ section_03_printing/printing_homework/printing_homework.test.cpp
@@ -48,6 +48,32 @@ TEST(Homework4Test, outputs_diamond_stars)
               "\n      * * *      \n        *        \n");
 }
 
+TEST(Homework5Test, find_all_errors_and_fix_them)
+{
+    testing::internal::CaptureStdout();
+    homework_5();
+    string result = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(result,
+              "work smart not hard\n"
+              "Children must be taught how to think, not what to think\n"
+              "We worry about what a child will become \" tomorrow \", yet we "
+              "forget that he is someone today\n"
+              "Children are not things to be molded"
+              ", but are people to be unfolded\n"
+              "Each day of our lives we make deposits in the memory "
+              "banks of our "
+              "children.\n"
+              "\" It is easier to build strong children than to repair "
+              "broken "
+              "men \""
+              "\n"
+              "Children need models rather than critics\n"
+              "Children have never been very good at listening to their "
+              "elders, "
+              "but they have never failed to imitate them"
+              "Children are our most valuable resource\n");
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
