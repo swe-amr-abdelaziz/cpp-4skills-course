27300d1cc6510cce177cdba197f3f82c61bab00a
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-18 12:23:46 +0300

    Add getFunctionOutput function to variables homework 2 unit testing file

---
+++ section_04_data_types_and_variables/variables_homework_2/variables_homework_2.test.cpp
@@ -0,0 +1,14 @@
+#include "variables_homework_2.h"
+#include <gtest/gtest.h>
+using namespace std;
+
+string getFunctionOutput(void (*func)(), string input)
+{
+    istringstream inputBuffer(input);
+    streambuf* cinBuffer = cin.rdbuf(inputBuffer.rdbuf());
+    testing::internal::CaptureStdout();
+    func();
+    cin.rdbuf(cinBuffer);
+    return testing::internal::GetCapturedStdout();
+}
+
