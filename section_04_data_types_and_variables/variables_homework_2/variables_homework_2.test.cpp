723f67b5d907dc47a1ef16f550736d87a56f7957
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-18 12:24:18 +0300

    Add variables homework 2 - problem 1 unit testing

--- section_04_data_types_and_variables/variables_homework_2/variables_homework_2.test.cpp
+++ section_04_data_types_and_variables/variables_homework_2/variables_homework_2.test.cpp
@@ -12,3 +12,20 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Problem1Test, guess_the_output)
+{
+    testing::internal::CaptureStdout();
+    problem_1();
+    string result = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(result,
+              "1\n"
+              "2\n"
+              "3\n"
+              "5\n"
+              "8\n"
+              "13\n"
+              "21\n"
+              "34\n"
+              "55\n");
+}
+
