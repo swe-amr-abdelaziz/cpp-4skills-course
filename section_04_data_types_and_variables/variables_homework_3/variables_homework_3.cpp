29a23529db55b86064878aac9c78e40c8ab13ad7
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-28 19:59:36 +0300

    Add variables homework 3 - problem 2 solution

--- section_04_data_types_and_variables/variables_homework_3/variables_homework_3.cpp
+++ section_04_data_types_and_variables/variables_homework_3/variables_homework_3.cpp
@@ -15,3 +15,19 @@ void problem_1()
     cout << num1 << " " << num2 << " " << num3 << "\n";
 }
 
+void problem_2()
+{
+    // Print Me
+    int a, b;
+
+    cin >> a >> b;
+
+    int b_equals_1 = (b + 1) / 2;
+    // If B = 1
+    int formula1 = (a * a) * b_equals_1;
+    // If B = -1
+    int formula2 = (2 * a + 1) * (1 - b_equals_1);
+
+    cout << formula1 + formula2;
+}
+
