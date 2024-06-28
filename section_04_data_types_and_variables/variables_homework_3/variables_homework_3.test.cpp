90a598a7fbb1b37ee7fabf5a6355dc99e41df694
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-28 20:03:19 +0300

    Add variables homework 3 - problem 2 unit testing

--- section_04_data_types_and_variables/variables_homework_3/variables_homework_3.test.cpp
+++ section_04_data_types_and_variables/variables_homework_3/variables_homework_3.test.cpp
@@ -37,3 +37,39 @@ TEST(Problem1Test, input_3_numbers_then_swap_them_4)
     EXPECT_EQ(output, "123 123 123\n");
 }
 
+TEST(Problem2Test, input_b_is_negative_one_test_case_1)
+{
+    string output = getFunctionOutput(problem_2, "10 -1");
+    EXPECT_EQ(output, "21");
+}
+
+TEST(Problem2Test, input_b_is_negative_one_test_case_2)
+{
+    string output = getFunctionOutput(problem_2, "0 -1");
+    EXPECT_EQ(output, "1");
+}
+
+TEST(Problem2Test, input_b_is_negative_one_test_case_3)
+{
+    string output = getFunctionOutput(problem_2, "-1000 -1");
+    EXPECT_EQ(output, "-1999");
+}
+
+TEST(Problem2Test, input_b_is_positive_one_test_case_1)
+{
+    string output = getFunctionOutput(problem_2, "10 1");
+    EXPECT_EQ(output, "100");
+}
+
+TEST(Problem2Test, input_b_is_positive_one_test_case_2)
+{
+    string output = getFunctionOutput(problem_2, "0 1");
+    EXPECT_EQ(output, "0");
+}
+
+TEST(Problem2Test, input_b_is_positive_one_test_case_3)
+{
+    string output = getFunctionOutput(problem_2, "-1000 1");
+    EXPECT_EQ(output, "1000000");
+}
+
