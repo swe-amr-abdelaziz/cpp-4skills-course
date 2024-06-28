34949f38cc6d6294cbe3addb8a341b34857ca026
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-28 20:03:02 +0300

    Add variables homework 3 - problem 1 unit testing

--- section_04_data_types_and_variables/variables_homework_3/variables_homework_3.test.cpp
+++ section_04_data_types_and_variables/variables_homework_3/variables_homework_3.test.cpp
@@ -13,3 +13,27 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Problem1Test, input_3_numbers_then_swap_them_1)
+{
+    string output = getFunctionOutput(problem_1, "115 20 301");
+    EXPECT_EQ(output, "20 301 115\n");
+}
+
+TEST(Problem1Test, input_3_numbers_then_swap_them_2)
+{
+    string output = getFunctionOutput(problem_1, "123 456 789");
+    EXPECT_EQ(output, "456 789 123\n");
+}
+
+TEST(Problem1Test, input_3_numbers_then_swap_them_3)
+{
+    string output = getFunctionOutput(problem_1, "123 123 456");
+    EXPECT_EQ(output, "123 456 123\n");
+}
+
+TEST(Problem1Test, input_3_numbers_then_swap_them_4)
+{
+    string output = getFunctionOutput(problem_1, "123 123 123");
+    EXPECT_EQ(output, "123 123 123\n");
+}
+
