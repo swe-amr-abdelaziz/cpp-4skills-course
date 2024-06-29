80acfc68adfbcec034c2e6051b0c2211fa9bb22b
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-29 11:52:59 +0300

    Add operators homework - problem 3 unit testing

--- section_05_operators/operators_homework/operators_homework.test.cpp
+++ section_05_operators/operators_homework/operators_homework.test.cpp
@@ -23,3 +23,22 @@ TEST(Problem2Test, guess_program_output)
     EXPECT_EQ(result, "8\n");
 }
 
+TEST(Problem3Test, guess_program_output)
+{
+    testing::internal::CaptureStdout();
+    problem_3();
+    string result = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(result,
+              "105\n"
+              "35\n"
+              "7\n"
+              "1\n"
+              "1\n"
+              "20\n"
+              "22\n"
+              "22\n"
+              "20\n"
+              "31\n"
+              "11\n"
+              "22\n");
+}
