728f64b15ba6a2ea4ea6f43a3534e775122775de
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-29 11:51:29 +0300

    Add operators homework - problem 2

--- section_05_operators/operators_homework/operators_homework.cpp
+++ section_05_operators/operators_homework/operators_homework.cpp
@@ -21,3 +21,23 @@ void problem_1()
     cout << a << "\n";
 }
 
+void problem_2()
+{
+    // Guess Program Output
+    int a = 1, b = 1, c;
+
+    cout << (c = a + b,
+             a = b,
+             b = c, // c = 2, a = 1, b = 2
+             c = a + b,
+             a = b,
+             b = c, // c = 3, a = 2, b = 3
+             c = a + b,
+             a = b,
+             b = c, // c = 5, a = 3, b = 5
+             c = a + b,
+             a = b,
+             b = c) // c = 8, a = 5, b = 8
+         << endl;
+}
+
