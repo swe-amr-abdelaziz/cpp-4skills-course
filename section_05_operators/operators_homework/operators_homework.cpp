b5653bf8891d11759d15639d113d960a3f2464e7
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-06-29 11:51:36 +0300

    Add operators homework - problem 3

--- section_05_operators/operators_homework/operators_homework.cpp
+++ section_05_operators/operators_homework/operators_homework.cpp
@@ -41,3 +41,32 @@ void problem_2()
          << endl;
 }
 
+void problem_3()
+{
+    // Guess Program Output
+    int a = 210;
+
+    a /= 2; // a = 105
+    cout << a << "\n";
+
+    cout << (a /= 3) << "\n"; // a = 35
+    cout << (a /= 5) << "\n"; // a = 7
+    cout << (a /= 7) << "\n"; // a = 1
+
+    cout << (2 + 3) * (5 - (-3)) / 5 / 8 << "\n";
+
+    a = 10;
+    cout << a++ + 10 << "\n"; // a = 11
+    cout << ++a + 10 << "\n"; // a = 12
+    cout << a-- + 10 << "\n"; // a = 11
+    cout << --a + 10 << "\n"; // a = 10
+
+    int b = 20;
+    cout << a++ + ++b << "\n"; // Don't code this way
+                               // a = 11, b = 21
+
+    cout << a << "\n";
+    ++a += 10; // Don't code this way
+               // a = 22
+    cout << a << "\n";
+}
