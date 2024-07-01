a61fefba4bae6510f580cb0066291bd802cd3977
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-01 07:05:32 +0300

    Add logical operators homework - homework 1 unit testing

--- section_05_operators/logical_operators_homework/logical_operators_homework.test.cpp
+++ section_05_operators/logical_operators_homework/logical_operators_homework.test.cpp
@@ -13,3 +13,22 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Homework1Test, guess_program_output)
+{
+    testing::internal::CaptureStdout();
+    homework_1();
+    string result = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(result,
+              "1\n"
+              "0\n"
+              "1\n"
+              "0\n"
+              "1\n"
+              "1\n"
+              "1\n"
+              "1\n"
+              "0\n"
+              "1\n"
+              "0\n");
+}
+
