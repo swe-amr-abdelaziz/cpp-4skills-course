00404b94f578c3eed8cfb22f6213f162d0fd0d45
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-01 07:04:42 +0300

    Add getFunctionOutput function to logical operators homework unit testing file

---
+++ section_05_operators/logical_operators_homework/logical_operators_homework.test.cpp
@@ -0,0 +1,15 @@
+#include "logical_operators_homework.h"
+#include <gtest/gtest.h>
+#include <sstream>
+using namespace std;
+
+string getFunctionOutput(void (*func)(), string input)
+{
+    istringstream inputBuffer(input);
+    streambuf* cinBuffer = cin.rdbuf(inputBuffer.rdbuf());
+    testing::internal::CaptureStdout();
+    func();
+    cin.rdbuf(cinBuffer);
+    return testing::internal::GetCapturedStdout();
+}
+
