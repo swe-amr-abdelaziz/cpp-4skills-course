fbe93b977fcbb6431c1bae42bd0da3a40ecd761e
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-11 19:55:26 +0300

    Add division and modulus homework 1 - problem 1 unit testing

--- section_05_operators/division_and_modulus_homework_1/division_and_modulus_homework_1.test.cpp
+++ section_05_operators/division_and_modulus_homework_1/division_and_modulus_homework_1.test.cpp
@@ -15,3 +15,38 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+void runProblem1Tests(string input, vector<double> expected)
+{
+    string output = getFunctionOutput(problem_1, input);
+    istringstream buffer(output);
+    string str;
+    vector<double> actual = {};
+    while (getline(buffer, str, '\n'))
+    {
+        actual.push_back(stod(str.c_str()));
+    }
+    EXPECT_DOUBLE_EQ(actual[0], expected[0]);
+    EXPECT_DOUBLE_EQ(actual[1], expected[1]);
+    EXPECT_DOUBLE_EQ(actual[2], expected[2]);
+}
+
+TEST(Problem1Test, averages_test_case_1)
+{
+    runProblem1Tests("1 2 3 4 5", {3, 0.666667, 0.444444});
+}
+
+TEST(Problem1Test, averages_test_case_2)
+{
+    runProblem1Tests("5 4 3 2 1", {3, 4, 2.66667});
+}
+
+TEST(Problem1Test, averages_test_case_3)
+{
+    runProblem1Tests("6 20 4 8 2", {8, 3, 2});
+}
+
+TEST(Problem1Test, averages_test_case_4)
+{
+    runProblem1Tests("0 20 4 8 2", {6.8, 2.4, 1.6});
+}
+
