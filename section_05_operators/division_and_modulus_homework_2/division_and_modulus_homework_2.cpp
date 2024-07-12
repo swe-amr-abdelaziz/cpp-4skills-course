e44b247def83edefdcfb09f1038b2bc00e7203c3
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-12 16:10:05 +0300

    Add division and modulus homework 2 - problem 1 way 2

--- section_05_operators/division_and_modulus_homework_2/division_and_modulus_homework_2.cpp
+++ section_05_operators/division_and_modulus_homework_2/division_and_modulus_homework_2.cpp
@@ -11,3 +11,14 @@ void problem_1_way_1()
     cout << is_even;
 }
 
+void problem_1_way_2()
+{
+    // Is even? way 2
+    double num;
+    cin >> num;
+
+    double result = num / 2;
+    bool is_even  = (result == (int) result);
+    cout << is_even;
+}
+
