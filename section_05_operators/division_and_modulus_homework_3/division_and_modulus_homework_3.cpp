d3426686d3b8727e4b51761b3aebe5bdf83be997
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-14 07:40:22 +0300

    Add division and modulus homework 3 - problem 2

--- section_05_operators/division_and_modulus_homework_3/division_and_modulus_homework_3.cpp
+++ section_05_operators/division_and_modulus_homework_3/division_and_modulus_homework_3.cpp
@@ -13,3 +13,18 @@ void problem_1()
     cout << result;
 }
 
+void problem_2()
+{
+    // Years!
+    const int DAYS_PER_MONTH = 30;
+    const int MONTHS_PER_YEAR = 12;
+    int days;
+    cin >> days;
+    
+    int years = days / (DAYS_PER_MONTH * MONTHS_PER_YEAR);
+    days %= (DAYS_PER_MONTH * MONTHS_PER_YEAR);
+    int months = days / DAYS_PER_MONTH;
+    days %= DAYS_PER_MONTH;
+
+    cout << years << ' ' << months << ' ' << days;
+}
