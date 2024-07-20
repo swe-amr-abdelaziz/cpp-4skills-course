23972f2225e282e33359ff587387794e9e03be34
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-20 14:08:00 +0300

    Add selection homework 2 - problem 1 unit testing

--- section_06_selection/selection_homework_2/selection_homework_2.test.cpp
+++ section_06_selection/selection_homework_2/selection_homework_2.test.cpp
@@ -14,3 +14,16 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Problem1Test, given_10_numbers_then_print_max_number_test_case_1)
+{
+    int output =
+        stoi(getFunctionOutput(problem1, "1 67 -9 88 -45 129 90 65 77 34"));
+    EXPECT_EQ(output, 129);
+}
+
+TEST(Problem1Test, given_10_numbers_then_print_max_number_test_case_2)
+{
+    int output = stoi(getFunctionOutput(problem1, "1 2 3 4 5 6 7 8 9 10"));
+    EXPECT_EQ(output, 10);
+}
+
