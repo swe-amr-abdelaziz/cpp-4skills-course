29407e107b7737f512b8e61e4f9bdf92e7902658
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-20 14:07:39 +0300

    Add getFunctionOutput function to selection homework 2 unit testing file

---
+++ section_06_selection/selection_homework_2/selection_homework_2.test.cpp
@@ -0,0 +1,16 @@
+#include "selection_homework_2.h"
+#include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+using namespace std;
+
+string getFunctionOutput(void (*func)(), string input)
+{
+    istringstream inputBuffer(input);
+    streambuf* cinBuffer = cin.rdbuf(inputBuffer.rdbuf());
+    testing::internal::CaptureStdout();
+    func();
+    cin.rdbuf(cinBuffer);
+    return testing::internal::GetCapturedStdout();
+}
+
