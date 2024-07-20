2ce87c13a3fe46e8204180e903e0fda23a5052dc
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-20 14:07:08 +0300

    Add selection homework 2 - problem 2

--- section_06_selection/selection_homework_2/selection_homework_2.cpp
+++ section_06_selection/selection_homework_2/selection_homework_2.cpp
@@ -39,3 +39,68 @@ void problem1()
     cout << max;
 }
 
+void problem2()
+{
+    // Find Maximum up to 10
+    int n, max, current;
+    cin >> n >> max;
+    n--;
+
+    // Using while loop is a better approach
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+    if (n--)
+    {
+        cin >> current;
+        if (max < current)
+            max = current;
+    }
+
+    cout << max;
+}
