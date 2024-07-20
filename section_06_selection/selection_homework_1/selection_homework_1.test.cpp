c9e72490c2f493f109f90caab61a9addb19169c1
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-20 11:32:19 +0300

    Add selection homework 1 - problem 1 unit testing

--- section_06_selection/selection_homework_1/selection_homework_1.test.cpp
+++ section_06_selection/selection_homework_1/selection_homework_1.test.cpp
@@ -14,3 +14,27 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Problem1Test, given_1st_number_odd_2nd_number_odd_then_print_their_multiplication)
+{
+    double output = stod(getFunctionOutput(problem1, "5 7"));
+    EXPECT_EQ(output, 35);
+}
+
+TEST(Problem1Test, given_1st_number_even_2nd_number_even_then_print_their_division)
+{
+    double output = stod(getFunctionOutput(problem1, "12 2"));
+    EXPECT_EQ(output, 6);
+}
+
+TEST(Problem1Test, given_1st_number_odd_2nd_number_even_then_print_their_summation)
+{
+    double output = stod(getFunctionOutput(problem1, "5 6"));
+    EXPECT_EQ(output, 11);
+}
+
+TEST(Problem1Test, given_1st_number_even_2nd_number_odd_then_print_their_subtraction)
+{
+    double output = stod(getFunctionOutput(problem1, "12 3"));
+    EXPECT_EQ(output, 9);
+}
+
