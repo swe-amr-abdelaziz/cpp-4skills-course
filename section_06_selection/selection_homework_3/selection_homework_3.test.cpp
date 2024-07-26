ebd2bb377b2f34bc374492c9d6645d9749afb40f
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-07-26 16:03:45 +0300

    Add selection homework 3 - problem 1 unit testing

--- section_06_selection/selection_homework_3/selection_homework_3.test.cpp
+++ section_06_selection/selection_homework_3/selection_homework_3.test.cpp
@@ -14,3 +14,51 @@ string getFunctionOutput(void (*func)(), string input)
     return testing::internal::GetCapturedStdout();
 }
 
+TEST(Problem1Test, given_three_intervals_with_no_intersection)
+{
+    int output = stoi(getFunctionOutput(problem1, "10 100 200 100 101 120 170"));
+    EXPECT_EQ(output, 0);
+}
+
+TEST(Problem1Test, given_three_intervals_with_one_interval_intersection_in_middle)
+{
+    int output = stoi(getFunctionOutput(problem1, "10 5 15 100 101 120 170"));
+    EXPECT_EQ(output, 1);
+}
+
+TEST(Problem1Test, given_three_intervals_with_one_interval_intersection_in_edge)
+{
+    int output = stoi(getFunctionOutput(problem1, "10 10 15 100 101 120 170"));
+    EXPECT_EQ(output, 1);
+}
+
+TEST(Problem1Test, given_three_intervals_with_two_interval_intersection_in_middle)
+{
+    int output = stoi(getFunctionOutput(problem1, "7 1 10 5 6 4 40"));
+    EXPECT_EQ(output, 2);
+}
+
+TEST(Problem1Test, given_three_intervals_with_two_interval_intersection_in_edge)
+{
+    int output = stoi(getFunctionOutput(problem1, "7 1 7 5 6 7 40"));
+    EXPECT_EQ(output, 2);
+}
+
+TEST(Problem1Test, given_three_intervals_with_three_interval_intersection_in_middle)
+{
+    int output = stoi(getFunctionOutput(problem1, "10 5 15 6 100 3 30"));
+    EXPECT_EQ(output, 3);
+}
+
+TEST(Problem1Test, given_three_intervals_with_three_interval_intersection_in_edge)
+{
+    int output = stoi(getFunctionOutput(problem1, "10 10 15 6 10 10 30"));
+    EXPECT_EQ(output, 3);
+}
+
+TEST(Problem1Test, given_three_intervals_with_three_interval_intersection_as_points)
+{
+    int output = stoi(getFunctionOutput(problem1, "1 1 1 1 1 1 1"));
+    EXPECT_EQ(output, 3);
+}
+
