693f96726b596745a4eadc7fef55c19ad9ddb2e2
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-01-30 14:28:00 +0200

    Add while loops practice - problem 1 unit testing

--- section_07_loops/while_loops_practice/while_loops_pactice.test.cpp
+++ section_07_loops/while_loops_practice/while_loops_pactice.test.cpp
@@ -30,3 +30,35 @@ vector<T> getFunctionOutputSplitted(void (*func)(), string input, char delimiter
     return vec;
 }
 
+TEST(Problem1Test, given_positive_number_get_numbers_divisible_by_3_test_case_1)
+{
+    vector<string> output = getFunctionOutputSplitted<string>(problem_1, "12");
+    vector<int> expected  = {3, 6, 9, 12};
+    ASSERT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < expected.size(); i++)
+        EXPECT_EQ(stoi(output[i]), expected[i]);
+}
+
+TEST(Problem1Test, given_positive_number_get_numbers_divisible_by_3_test_case_2)
+{
+    vector<string> output = getFunctionOutputSplitted<string>(problem_1, "22");
+    vector<int> expected  = {3, 6, 9, 12, 15, 18, 21};
+    ASSERT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < expected.size(); i++)
+        EXPECT_EQ(stoi(output[i]), expected[i]);
+}
+
+TEST(Problem1Test, given_zero_get_nothing)
+{
+    vector<string> output = getFunctionOutputSplitted<string>(problem_1, "0");
+    vector<int> expected  = {};
+    EXPECT_EQ(output.size(), expected.size());
+}
+
+TEST(Problem1Test, given_negative_number_get_nothing)
+{
+    vector<string> output = getFunctionOutputSplitted<string>(problem_1, "-20");
+    vector<int> expected  = {};
+    EXPECT_EQ(output.size(), expected.size());
+}
+
