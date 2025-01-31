7f5519bbbc25095f894aca303cd704dcc3b6a160
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-01-31 21:56:56 +0200

    Add while loops homework 2 - problem 1 unit testing

--- section_07_loops/while_loops_homework_2/while_loops_homework_2.test.cpp
+++ section_07_loops/while_loops_homework_2/while_loops_homework_2.test.cpp
@@ -27,3 +27,46 @@ vector<T> getFunctionOutputSplitted(void (*func)(), string input, char delimiter
     return vec;
 }
 
+TEST(Problem1Test, given_number_prints_diamond_test_case_1)
+{
+    vector<string> output   = getFunctionOutputSplitted<string>(problem_1, "5");
+    vector<string> expected = {
+        "    *",
+        "   ***",
+        "  *****",
+        " *******",
+        "*********",
+        "*********",
+        " *******",
+        "  *****",
+        "   ***",
+        "    *",
+    };
+    ASSERT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < expected.size(); i++)
+        EXPECT_EQ(output[i], expected[i]);
+}
+
+TEST(Problem1Test, given_number_prints_diamond_test_case_2)
+{
+    vector<string> output   = getFunctionOutputSplitted<string>(problem_1, "3");
+    vector<string> expected = {
+        "  *",
+        " ***",
+        "*****",
+        "*****",
+        " ***",
+        "  *",
+    };
+    ASSERT_EQ(output.size(), expected.size());
+    for (size_t i = 0; i < expected.size(); i++)
+        EXPECT_EQ(output[i], expected[i]);
+}
+
+TEST(Problem1Test, given_number_prints_diamond_test_case_3)
+{
+    vector<string> output   = getFunctionOutputSplitted<string>(problem_1, "0");
+    vector<string> expected = {};
+    EXPECT_EQ(output.size(), expected.size());
+}
+
