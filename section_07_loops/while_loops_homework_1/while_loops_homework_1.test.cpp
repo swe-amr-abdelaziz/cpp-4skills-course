b1ad4637e9cda1f21e5908c96bb4ada6c9470d47
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-01-31 10:05:20 +0200

    Add while loops homework 1 - unit testing helper functions

---
+++ section_07_loops/while_loops_homework_1/while_loops_homework_1.test.cpp
@@ -0,0 +1,30 @@
+#include "while_loops_homework_1.h"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string getFunctionOutput(void (*func)(), string input)
+{
+    istringstream inputBuffer(input);
+    streambuf* cinBuffer = cin.rdbuf(inputBuffer.rdbuf());
+    testing::internal::CaptureStdout();
+    func();
+    cin.rdbuf(cinBuffer);
+    return testing::internal::GetCapturedStdout();
+}
+
+template <typename T>
+vector<T> getFunctionOutputSplitted(void (*func)(), string input, char delimiter = '\n')
+{
+    string output = getFunctionOutput(func, input);
+    istringstream buffer(output);
+    string str;
+    vector<T> vec = {};
+    while (getline(buffer, str, delimiter))
+        vec.push_back(str.c_str());
+    return vec;
+}
+
