9253d8dc0ce92f6779d0486c280b6aab2fd8102d
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-01-31 10:02:16 +0200

    Add while loops homework 1 - problem 1

---
+++ section_07_loops/while_loops_homework_1/while_loops_homework_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void problem_1()
+{
+    // Print Range
+    int x;
+    int y;
+    cin >> x >> y;
+    int step = x < y ? 1 : -1;
+
+    while (x != y)
+    {
+        cout << x << '\n';
+        x += step;
+    }
+    cout << y << '\n';
+}
+
